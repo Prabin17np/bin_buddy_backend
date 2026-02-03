ab78e081fa48931493e2277013de9faa00f69512
Prabin Giri <prabin17np@gmail.com>
2026-02-03 17:56:21 +0545

    implement admin user CRUD business logic

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,5 +1,5 @@
 import { UserRepository } from "../repository/user.repository";
-import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
+import { CreateUserDTO, LoginUserDTO, UpdateUserDTO } from "../dtos/user.dto";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config";
@@ -50,5 +50,47 @@ export class UserService{
 
     const token = jwt.sign(payload, JWT_SECRET, {expiresIn: "30d"});
     return {token, user};
+  }
+
+    async updateUser(id:string, data:UpdateUserDTO) {
+    const user = await userRepository.getUserById(id);
+    if(!user) {
+      throw new HttpError(404, "User not found");
+    }
+    // if(user.email != data.email) {
+    //   const emailCheck = await userRepository.getUserByEmail(data.email!);
+    //   if(!emailCheck) {
+    //     throw new HttpError(403, "Email already in use");
+    //   }
+    // }
+    // if(user.username != data.username) {
+    //   const usernameCheck = await userRepository.getUserByUsername(data.username!);
+    //   if(!usernameCheck) {
+    //     throw new HttpError(403, "Username already in use");
+    //   }
+    // }
+
+    if(data.password){
+      const hashedPassword = await bcryptjs.hash(data.password, 10);
+      data.password   = hashedPassword;
+    }
+
+    const updatedUser = await userRepository.updateUser(id, data);
+    return updatedUser;
+  }
+
+    async uploadProfilePicture(file: Express.Multer.File) {
+    if (!file) {
+    throw new Error("Please upload a file");
+  }
+
+  if (file.size > Number(process.env.MAX_FILE_UPLOAD)) {
+    throw new Error(
+      `Please upload an image less than ${process.env.MAX_FILE_UPLOAD} bytes`
+    );
+  }
+
+  const filename = await userRepository.uploadProfilePicture(file);
+  return filename;
   }
 }
