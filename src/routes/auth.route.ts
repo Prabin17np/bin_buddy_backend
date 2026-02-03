e65640697cf2fddbbd59e4742e92f05fe1f754b7
Prabin Giri <prabin17np@gmail.com>
2026-02-03 17:56:50 +0545

    add admin user routes with role-based middleware

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authorizedMiddleware } from "../middleware/authorized.middleware";
+import { uploads } from "../middleware/upload.middleware";
 
 let authController = new AuthController();
 const router = Router();
 
 router.post("/login", authController.login);
 router.post("/register", authController.register);
+
+router.put(
+  "/update-profile",
+  authorizedMiddleware,
+  uploads.single("profilePicture"), // info: image => filename in form data
+  authController.updateUser
+);
+
+router.post("/upload-image", authorizedMiddleware, uploads.single("profilePicture"), authController.uploadProfilePicture);
+
 
 export default router;
